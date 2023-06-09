d9927c19f27568c130a99563550ae0212c1ba967
wesiudev <wesselpawel@wp.pl>
2023-06-09 21:19:57 +0200

    blog & slug

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 import { GalleryGrid } from "./components/GalleryGrid";
 import { Announcer } from "./components/Announcer";
 import { Reserve } from "./components/Reserve";
-import { ServicesGrid } from "./components/ServicesGrid";
 import { getServicesList } from "./lib/getServicesList";
 
 export default async function Home() {
-  const { services } = await getServicesList();
   return (
     <>
       <GalleryGrid />
       <Announcer />
-      <ServicesGrid services={services} />
       <Reserve />
     </>
   );
