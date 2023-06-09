4e472b89d515c7b3b35d2a530ae25bf316f574c3
wesiudev <wesselpawel@wp.pl>
2023-06-09 23:51:32 +0200

    blog & slug

--- app/lib/getPostBySlug.tsx
+++
@@ -1,8 +0,0 @@
-"use server";
-export async function getPostBySlug(postId: string) {
-  const data = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/blog/${postId}`
-  );
-
-  return data.json();
-}
