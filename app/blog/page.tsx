6a5919f1471964724bc1a248a6ae424c298879f8
wesiudev <wesselpawel@wp.pl>
2023-06-09 21:14:52 +0200

    blog & slug

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,7 @@
-import { getBlogData } from "../lib/getBlogData";
-import { RecentPosts } from "./components/RecentPosts";
 export default async function Blog() {
-  const posts = await getBlogData();
   return (
     <div className="w-[90vw] mx-auto">
-      <div className="lg:w-1/2">
-        <RecentPosts posts={posts.posts} />
-      </div>
+      <div className="lg:w-1/2"></div>
     </div>
   );
 }
