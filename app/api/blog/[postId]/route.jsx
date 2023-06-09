c51293b0b322c27f90d971643d19e811bbb330f1
wesiudev <wesselpawel@wp.pl>
2023-06-09 21:42:50 +0200

    blog & slug

--- app/api/blog/[postId]/route.jsx
+++
@@ -1,8 +0,0 @@
-import { getBlogPosts } from "@/firebase";
-import { NextResponse } from "next/server";
-export async function GET(req, { params }) {
-  const posts = await getBlogPosts();
-  const post = posts.posts.find((post) => post.postId === params.postId);
-
-  return NextResponse.json(post);
-}
