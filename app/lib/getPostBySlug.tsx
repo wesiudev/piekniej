86f3cb9a85c37292149ecd0b67f2d8c2be1c09e9
wesiudev <wesselpawel@wp.pl>
2023-06-09 23:13:05 +0200

    blog & slug

--- app/lib/getPostBySlug.tsx
+++ app/lib/getPostBySlug.tsx
@@ -4,9 +4,5 @@ export async function getPostBySlug(postId: string) {
     `${process.env.NEXT_PUBLIC_SITE_URL}/api/blog/${postId}`
   );
 
-  if (!data.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
   return data.json();
 }
