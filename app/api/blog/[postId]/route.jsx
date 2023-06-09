e2a6a7f31ec01e6f04dc0808d982b32160a33a38
wesiudev <wesselpawel@wp.pl>
2023-06-09 21:41:48 +0200

    blog & slug

--- app/api/blog/[postId]/route.jsx
+++ app/api/blog/[postId]/route.jsx
@@ -1,18 +1,8 @@
 import { getBlogPosts } from "@/firebase";
 import { NextResponse } from "next/server";
 export async function GET(req, { params }) {
-  try {
-    const posts = await getBlogPosts();
-    const post = posts.posts.find((post) => post.postId === params.postId);
-    console.log(post);
-    if (!post) {
-      return new NextResponse("not found", { status: 404 });
-    }
+  const posts = await getBlogPosts();
+  const post = posts.posts.find((post) => post.postId === params.postId);
 
-    return NextResponse.json({
-      post,
-    });
-  } catch (error) {
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
+  return NextResponse.json(post);
 }
