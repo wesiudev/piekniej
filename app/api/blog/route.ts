46a3dbd91130e84b132902bc9797d68f97581796
wesiudev <wesselpawel@wp.pl>
2023-06-09 23:37:25 +0200

    blog & slug

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -2,6 +2,10 @@ import { getBlogPosts } from "@/firebase/"
 import { NextResponse } from "next/server";
 
 export async function GET() {
+    try {
     const posts = await getBlogPosts();
     return NextResponse.json(posts)
+} catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
