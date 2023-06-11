4418ed63efd43dab2dce50419dd29f29ebc92ec9
wesiudev <wesselpawel@wp.pl>
2023-06-11 20:44:05 +0200

    utf-8

--- app/lib/getServicesList.tsx
+++ app/lib/getServicesList.tsx
@@ -1,6 +1,6 @@
 "use server";
 export async function getServicesList() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/data`);
+  const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
 
   const services = req.json();
   return services;
