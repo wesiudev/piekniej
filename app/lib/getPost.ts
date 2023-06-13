eea8d5a25c3c4a9cd6d307e907c921236b29aa76
wesiudev <wesselpawel@wp.pl>
2023-06-13 21:42:07 +0200

    import fix

--- app/lib/getPost.ts
+++ app/lib/getPost.ts
@@ -1,7 +1,7 @@
 "use server";
 export async function getPost(postId: string) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SITE_URL}/api/blog/${postId}`
+    `https://manicuregrudziadz.pl/api/blog/${postId}`
   );
   if (!res) {
     throw new Error("Failed to fetch data");
