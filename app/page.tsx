b37eee90c56c30e61b34915a50e20924ce202fb1
wesiudev <wesselpawel@wp.pl>
2023-07-28 20:46:36 +0200

    carousel

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,12 @@ import { Reserve } from "./components/Reserve";
 import { ServicesGrid } from "./components/ServicesGrid";
 import BlogInviter from "./components/BlogInviter";
 import AboutQuality from "./components/carousel/AboutQuality";
-import Carousel from "./components/carousel/Carousel";
+import { carousel } from "@/public/carousel.json";
 async function getServicesList() {
   const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/services`);
 
   const services = req.json();
   return services;
-}
-async function getCarouselData() {
-  const req = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/carousel`);
-
-  const carousel = req.json();
-  return carousel;
 }
 
 async function getBlogData() {
@@ -26,7 +20,6 @@ async function getBlogData() {
 
 export default async function Home() {
   const { services } = await getServicesList();
-  const { carousel } = await getCarouselData();
   const { posts } = await getBlogData();
   return (
     <>
@@ -34,7 +27,7 @@ export default async function Home() {
       <Announcer />
       <ServicesGrid services={services} />
       <Reserve />
-      <Carousel carousel={carousel} />
+      <AboutQuality carousel={carousel} />
       <BlogInviter posts={posts.posts} />
     </>
   );
